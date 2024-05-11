e7396242090cdd36fef90175b109f227c907e34a
arijghrs <arijgharsalli8@gmail.com>
2024-05-11 03:01:24 +0100

    upt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RoomComponent } from './components/room/room.component';
 
 export const routes: Routes = [
-    {path: 'app-home', component: HomeComponent}
+    {path: 'app-home', component: HomeComponent},
+    {path: 'app-room', component: RoomComponent},
+    
 ];
