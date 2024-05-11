9e11aa9fe5fe76647ea5a6d0235d0818915d23e9
manelaskry <manlaskry8@gmail.com>
2024-05-11 03:16:26 +0200

    login-tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {SigninComponent} from './signin/signin.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'app-signin', component: SigninComponent },
+];
