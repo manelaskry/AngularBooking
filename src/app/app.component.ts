73ab14e86169119893c7880be63a457681eadf4c
manelaskry <manlaskry8@gmail.com>
2024-05-11 03:22:34 +0200

    architecture-upt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SigninComponent } from './signin/signin.component';
+import { SigninComponent } from './pages/signin/signin.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import {SigninComponent} from './signin/signin.component';
+import {SigninComponent} from './pages/signin/signin.component';
 
 
 export const routes: Routes = [
