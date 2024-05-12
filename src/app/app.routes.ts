9594e968d4500ea39ae1c57dafee54f87a01ad46
manelaskry <manlaskry8@gmail.com>
2024-05-12 17:13:28 +0200

    Reservation interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import {SigninComponent} from './pages/signin/signin.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
 
 
 export const routes: Routes = [
     { path: 'app-signin', component: SigninComponent },
     { path: 'app-home', component: HomeComponent},
+    { path: 'app-reservation', component:ReservationComponent},
    
 ];
